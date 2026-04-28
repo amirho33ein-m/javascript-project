ebcf0b38a36d8417aaa2641d01aafdf9d446f64e
Amir Hossein <amirhossein138103@gmail.com>
2026-04-28 17:24:50 -0600

    create clock program

---
+++ sources/clock/assets/app.js
@@ -0,0 +1,25 @@
+const hourHand = document.querySelector("#hourHand");
+const minuteHand = document.querySelector("#minuteHand");
+const secondHand = document.querySelector("#secondHand");
+const digitalTime = document.querySelector("#digitalTime");
+
+const loadPage = () => {
+    setInterval(() => {
+        const setTime = (() => {
+            const getDate = new Date();
+            
+            let second = getDate.getSeconds();    
+            let minute = getDate.getMinutes();    
+            let hour = getDate.getHours();
+
+            digitalTime.textContent = `${hour > 10 ? "" : "0"}${hour}:${minute > 10 ? "" : "0"}${minute}:${second > 10 ? "" : "0"}${second}`;
+            hourHand.style.rotate = `${hour * 30 + minute / 2}deg`;
+            minuteHand.style.rotate = `${minute * 6}deg`;
+            secondHand.style.rotate = `${second * 6}deg`;
+        })();
+    }, 1000);
+};
+
+
+
+window.addEventListener("load", loadPage)
