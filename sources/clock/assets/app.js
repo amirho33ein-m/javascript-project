c729ab1a1f9e09c4abd59272721a2aab015b272e
Amir Hossein <amirhossein138103@gmail.com>
2026-04-28 21:14:55 -0600

    modify clock template

--- sources/clock/assets/app.js
+++ sources/clock/assets/app.js
@@ -2,21 +2,21 @@ const hourHand = document.querySelector("#hourHand");
 const minuteHand = document.querySelector("#minuteHand");
 const secondHand = document.querySelector("#secondHand");
 const digitalTime = document.querySelector("#digitalTime");
+const showTime = document.querySelector(".time");
 
 const loadPage = () => {
-    setInterval(() => {
-        const setTime = (() => {
-            const getDate = new Date();
-            
-            let second = getDate.getSeconds();    
-            let minute = getDate.getMinutes();    
-            let hour = getDate.getHours();
 
-            digitalTime.textContent = `${hour > 10 ? "" : "0"}${hour}:${minute > 10 ? "" : "0"}${minute}:${second > 10 ? "" : "0"}${second}`;
-            hourHand.style.rotate = `${hour * 30 + minute / 2}deg`;
-            minuteHand.style.rotate = `${minute * 6}deg`;
-            secondHand.style.rotate = `${second * 6}deg`;
-        })();
+    setInterval(() => {
+        const getDate = new Date();
+        
+        let second = getDate.getSeconds();    
+        let minute = getDate.getMinutes();    
+        let hour = getDate.getHours();
+
+        showTime.textContent = `${hour >= 10 ? "" : "0"}${hour}:${minute >= 10 ? "" : "0"}${minute}:${second >= 10 ? "" : "0"}${second}`;
+        hourHand.style.rotate = `${hour * 30 + minute / 2}deg`;
+        minuteHand.style.rotate = `${minute * 6}deg`;
+        secondHand.style.rotate = `${second * 6}deg`;
     }, 1000);
 };
 
